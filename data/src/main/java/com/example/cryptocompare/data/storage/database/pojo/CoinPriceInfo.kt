72a0e9ffa1febc9acb3fc2b69b8c195543291422
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-26 13:38:09 +0400

    feat(data/storage/database): add pojo objects

---
+++ data/src/main/java/com/example/cryptocompare/data/storage/database/pojo/CoinPriceInfo.kt
@@ -0,0 +1,44 @@
+package com.example.cryptocompare.data.storage.database.pojo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+
+@Entity(tableName = "full_price_list")
+data class CoinPriceInfo(
+
+    @PrimaryKey
+    @SerializedName("FROMSYMBOL")
+    @Expose
+    val fromSymbol: String,
+
+    @SerializedName("TOSYMBOL")
+    @Expose
+    val toSymbol: String?,
+
+    @SerializedName("PRICE")
+    @Expose
+    val price: Double?,
+
+    @SerializedName("LASTUPDATE")
+    @Expose
+    val lastUpdate: Int?,
+
+    @SerializedName("HIGHDAY")
+    @Expose
+    val highDay: Double?,
+
+    @SerializedName("LOWDAY")
+    @Expose
+    val lowDay: Double?,
+
+    @SerializedName("LASTMARKET")
+    @Expose
+    val lastMarket: String?,
+
+    @SerializedName("IMAGEURL")
+    @Expose
+    val imageUrl: String?
+)
