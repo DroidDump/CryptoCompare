b9353f412be4ebf3c03ec52fcbc8a1d47fed565f
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-26 17:45:53 +0400

    feat(app): add 'API_KEY' for min-api.cryptocompare.com

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 Mobile client for [CryptoCompare](https://www.cryptocompare.com/)
 
 API used: [documentation](https://min-api.cryptocompare.com/documentation)
+
+### Requirements:
+
+In the project-level 'local.properties' file, you need to add the parameter:  
+`API_KEY=<YOUR API KEY>` (without quotes)
--- app/src/main/java/com/example/cryptocompare/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocompare/presentation/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.example.cryptocompare.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cryptocompare.R
+import com.example.cryptocompare.data.storage.database.api.ApiFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val disposable = ApiFactory.apiService.getFullPriceList(
+            apiKey = getString(R.string.api_key),
+            fSyms = "BTC,ETH,EOS"
+        )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            },{
+                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
