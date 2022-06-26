7fdc7355e3d570831cccfee1f9dfd79c4d0226b9
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-26 12:23:56 +0400

    feat(data/storage/database): add 'DBCoinStorage' & 'DBCoinListStorage'

---
+++ data/src/main/java/com/example/cryptocompare/data/storage/database/DBCoinListStorage.kt
@@ -0,0 +1,12 @@
+package com.example.cryptocompare.data.storage.database
+
+import com.example.cryptocompare.data.storage.CoinListStorage
+import com.example.cryptocompare.data.storage.models.Coin
+
+
+class DBCoinListStorage : CoinListStorage {
+
+    override fun get(): List<Coin> {
+        TODO("Not yet implemented")
+    }
+}
---
+++ data/src/main/java/com/example/cryptocompare/data/storage/database/DBCoinStorage.kt
@@ -0,0 +1,12 @@
+package com.example.cryptocompare.data.storage.database
+
+import com.example.cryptocompare.data.storage.CoinStorage
+import com.example.cryptocompare.data.storage.models.Coin
+
+
+class DBCoinStorage : CoinStorage {
+
+    override fun get(fromSymbol: String): Coin {
+        TODO("Not yet implemented")
+    }
+}
