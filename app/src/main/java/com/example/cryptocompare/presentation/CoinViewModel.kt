7694417ceb33e3b4a2aeb5c5f67919a489dfd73b
Aleksey Voko <voko.aleksey@gmail.com>
2022-06-28 11:22:23 +0400

    feat(app/presentation): add 'CoinViewModel'

---
+++ app/src/main/java/com/example/cryptocompare/presentation/CoinViewModel.kt
@@ -0,0 +1,75 @@
+package com.example.cryptocompare.presentation
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.cryptocompare.R
+import com.google.gson.Gson
+import com.example.cryptocompare.data.api.ApiFactory
+import com.example.cryptocompare.data.database.AppDatabase
+import com.example.cryptocompare.data.api.models.CoinPriceInfo
+import com.example.cryptocompare.data.api.models.CoinPriceInfoRawData
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+    private val apiKey = application.getString(R.string.api_key)
+    private val db = AppDatabase.getInstance(application)
+    private val compositeDisposable = CompositeDisposable()
+
+    val priceList = db.coinPriceInfoDao().getPriceList()
+
+    fun getDetailInfo(fSym: String): LiveData<CoinPriceInfo> {
+        return db.coinPriceInfoDao().getPriceInfoAboutCoin(fSym)
+    }
+
+    init {
+        loadData()
+    }
+
+    fun loadData() {
+        val disposable = ApiFactory.apiService.getTopCoinsInfo(apiKey = apiKey, limit = 50)
+            .map { it -> it.data?.map { it.coinInfo?.name }?.joinToString(",") }
+            .flatMap { ApiFactory.apiService.getFullPriceList(apiKey = apiKey, fSyms = it) }
+            .map { getPriceListFromRawData(it) }
+            .delaySubscription(10, TimeUnit.SECONDS)
+            .repeat()
+            .retry()
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                db.coinPriceInfoDao().insertPriceList(it)
+                Log.d("TEST_OF_LOADING_DATA", "Success: $it")
+            }, {
+                Log.d("TEST_OF_LOADING_DATA", "Failure: ${it.message.toString()}")
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    private fun getPriceListFromRawData(
+        coinPriceInfoRawData: CoinPriceInfoRawData
+    ): List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
--- app/src/main/java/com/example/cryptocompare/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocompare/presentation/MainActivity.kt
@@ -1,39 +1,20 @@
 package com.example.cryptocompare.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.cryptocompare.R
-import com.example.cryptocompare.data.api.ApiFactory
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val disposable = ApiFactory.apiService.getFullPriceList(
-            apiKey = getString(R.string.api_key),
-            fSyms = "BTC,ETH,EOS"
-        )
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d("TEST_OF_LOADING_DATA", it.toString())
-            },{
-                Log.d("TEST_OF_LOADING_DATA", it.message.toString())
-            })
-        compositeDisposable.add(disposable)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.loadData()
     }
 }
